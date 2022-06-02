2a9292931d4770a66e7a55d89379396a820a97b9
José Luis González <joseluisgonsan@gmail.com>
2022-06-02 11:07:57 +0200

    Leccion 1.2 Init

--- Leccion01/app/src/main/java/es/joseluisgs/leccion01/MainActivity.kt
+++ Leccion01/app/src/main/java/es/joseluisgs/leccion01/MainActivity.kt
@@ -2,10 +2,44 @@ package es.joseluisgs.leccion01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import es.joseluisgs.leccion01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Hacemos el binding de los elementos de la vista
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // sin view binding
+        //setContentView(R.layout.activity_main)
+
+        // Para el binding de la vista usando viewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(binding.root)
+
+
+
+        // Para obtener los elementos de la vista usamos findViewById
+        // Pero esto puede ocasionar problemas de nullPointerException
+        // val btRoll: Button = findViewById(R.id.btRoll)
+        // val tvResult: TextView = findViewById(R.id.tvResult)
+
+        // Haciendo el binding de los elementos de la vista usaremos el viewBinding
+        val btRoll = binding.btRoll
+        val tvResult = binding.tvResult
+
+        btRoll.setOnClickListener {
+            rollDice()
+            tvResult.text = (1..6).random().toString()
+        }
+
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "Dice rolled", Toast.LENGTH_SHORT).show()
     }
 }
