ea0e42c8b4ad8ab2d925dc751326328c2a1c708e
José Luis González <joseluisgonsan@gmail.com>
2022-06-06 18:39:16 +0200

    Leccion 2.3 Constraint

---
+++ Leccion02/app/src/main/java/es/joseluisgs/leccion02/ConstraintActivity.kt
@@ -0,0 +1,11 @@
+package es.joseluisgs.leccion02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
