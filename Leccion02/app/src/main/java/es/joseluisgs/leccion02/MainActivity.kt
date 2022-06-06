15264e59c117198a6f57ba4ecc4bb563ee9e69a2
José Luis González <joseluisgonsan@gmail.com>
2022-06-06 19:30:07 +0200

    Leccion 2.4 Fin

--- Leccion02/app/src/main/java/es/joseluisgs/leccion02/MainActivity.kt
+++ Leccion02/app/src/main/java/es/joseluisgs/leccion02/MainActivity.kt
@@ -4,37 +4,39 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import es.joseluisgs.leccion02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Binding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+
+        // podemos hacerlo así
+        // binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // O así
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Recuperamos el botón
-        val btnDone = findViewById<Button>(R.id.btnDone)
-        btnDone.setOnClickListener {
+        binding.btnDone.setOnClickListener {
             addNickname(it)
         }
 
         // Tambien lo podemos hacer de un tirón
-        findViewById<TextView>(R.id.tvNickname).setOnClickListener {
+        binding.tvNickname.setOnClickListener {
             updateNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        // Encontramos los elementos de la vista con la que vamos a trabajar
-        val editText = findViewById<EditText>(R.id.etNickname)
-        val nicknameTextView = findViewById<TextView>(R.id.tvNickname)
         // Asignamos el valor
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
+        binding.tvNickname.text = binding.etNickname.text
+        binding.etNickname.visibility = View.GONE
         view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.tvNickname.visibility = View.VISIBLE
         // Escondemos el teclado automáticamente
         // Hide the keyboard.
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -42,17 +44,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.etNickname)
-        val doneButton = findViewById<Button>(R.id.btnDone)
         // para cambiar la visibilidad
-        editText.visibility = View.VISIBLE
-        doneButton.visibility = View.VISIBLE
+        binding.etNickname.visibility = View.VISIBLE
+        binding.btnDone.visibility = View.VISIBLE
         view.visibility = View.GONE
 
         // Obtenemos el foco
-        editText.requestFocus()
+        binding.etNickname.requestFocus()
         // Mostramos el teclado
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(editText, 0)
+        imm.showSoftInput(binding.etNickname, 0)
     }
 }
