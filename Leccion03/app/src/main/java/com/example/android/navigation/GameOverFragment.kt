0169707f494417a1dcb53fbd559893d042a7d878
José Luis González <joseluisgonsan@gmail.com>
2022-06-16 17:21:31 +0200

    Leccion 3.2 Navegación en intentar de nuevo

--- Leccion03/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ Leccion03/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -22,14 +22,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_over, container, false)
+            inflater, R.layout.fragment_game_over, container, false
+        )
+
+        // Volvemos a la pantalla de inicio
+        binding.tryAgainButton.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(R.id.action_gameOverFragment_to_gameFragment)
+        }
+
         return binding.root
     }
 }
--- Leccion03/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ Leccion03/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -22,15 +22,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
 class GameWonFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_won, container, false)
+            inflater, R.layout.fragment_game_won, container, false
+        )
+
+        // Volvemos a la pantalla anterior
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(R.id.action_gameWonFragment_to_gameFragment)
+        }
+
         return binding.root
     }
 }
