9da2309f920056f0bfe2fbcaf8da9b48e9e2a39f
José Luis González <joseluisgonsan@gmail.com>
2022-06-16 11:16:27 +0200

    Binding en Main Activity

--- Leccion03/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ Leccion03/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -18,14 +18,17 @@ package com.example.android.navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Creamos el binding de la vista
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     // TODO (01) Create the new TitleFragment
