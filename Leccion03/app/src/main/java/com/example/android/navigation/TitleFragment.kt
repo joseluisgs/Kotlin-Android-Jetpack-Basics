bdb334d6b8f95114062c7c52108903acec75b097
José Luis González <joseluisgonsan@gmail.com>
2022-06-17 09:40:56 +0200

    Leccion 4.1 Ciclo de vida de fragment

--- Leccion03/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Leccion03/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,6 +1,8 @@
 package com.example.android.navigation
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -29,6 +31,9 @@ class TitleFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i("TitleFragment", "onCreate called")
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -43,6 +48,8 @@ class TitleFragment : Fragment() {
         // Aplicamos el binding
         _binding = FragmentTitleBinding.inflate(inflater, container, false)
 
+        Log.i("TitleFragment", "onCreateView called")
+
         // para que el fragment tenga un menu
         setHasOptionsMenu(true)
 
@@ -53,12 +60,44 @@ class TitleFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        Log.i("TitleFragment", "onViewCreated called")
+
         // Cuando la vista esta creada, creamos los eventos
         binding.playButton.setOnClickListener { view: View ->
             view.findNavController()
                 .navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
         }
 
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i("TitleFragment", "onAttach called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("TitleFragment", "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("TitleFragment", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("TitleFragment", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("TitleFragment", "onStop called")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.i("TitleFragment", "onDetach called")
     }
 
     // Muestra el menu
@@ -77,6 +116,7 @@ class TitleFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        Log.i("TitleFragment", "onDestroyView called")
     }
 
 
