87262dc1a9368aae9b248251ebb99996829c7f16
José Luis González <joseluisgonsan@gmail.com>
2022-06-17 10:14:57 +0200

    Leccion 4.2: Guardar estado de la actividad

--- Leccion04/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ Leccion04/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -27,6 +27,12 @@ import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
 import timber.log.Timber
 
+// Constantes en tiempo de compilación para el estado de la actividad
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+const val KEY_TIMER_SECONDS = "timer_seconds_key"
+
+
 class MainActivity : AppCompatActivity() {
 
     private var revenue = 0
@@ -63,12 +69,21 @@ class MainActivity : AppCompatActivity() {
     )
     private var currentDessert = allDesserts[0]
 
+    // Podemos recuperar el estado de la actividad en el onCreate(savedInstanceState: Bundle?)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Ciclo de vida
         Timber.i("onCreate Called")
         dessertTimer = DessertTimer(this.lifecycle) // Ahora observa el ciclo de vida de esta actividad
+
+        // Recuperamos el estado de la actividad
+        if (savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            dessertTimer.secondsCount =
+                savedInstanceState.getInt(KEY_TIMER_SECONDS, 0)
+        }
 
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -100,6 +115,18 @@ class MainActivity : AppCompatActivity() {
         //dessertTimer.stopTimer() // Ya no lo necesita porque hay un observador en el ciclo de vida
 
         Timber.i("onStop Called")
+    }
+
+    // Vamos a salvar el estadoo de la interfaz
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        Timber.i("onSaveInstanceState Called")
+        // Salvamos el estado de la actividad
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
+        outState.putInt(KEY_TIMER_SECONDS, dessertTimer.secondsCount)
+        showCurrentDessert()
     }
 
     override fun onResume() {
