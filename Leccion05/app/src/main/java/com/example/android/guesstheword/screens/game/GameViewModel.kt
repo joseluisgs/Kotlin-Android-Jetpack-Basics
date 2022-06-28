8b200a907d0ce9e190ea1f543a2e05eabdeaf498
José Luis González <joseluisgonsan@gmail.com>
2022-06-28 16:34:18 +0200

    Leccion 5.4 Transformation Map

--- Leccion05/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Leccion05/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 // View Model for GameFragment
@@ -47,6 +49,11 @@ class GameViewModel : ViewModel() {
 
     // Para controlar temporizador
     private val timer: CountDownTimer
+
+    // The String version of the current time
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
 
 
     init {
