206d6f2cc2e4fe96df4c86f256655169571f4efb
José Luis González <joseluisgonsan@gmail.com>
2022-06-28 12:44:51 +0200

    Leccion 5.2 Encapsulate the LiveData

--- Leccion05/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Leccion05/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -8,19 +9,23 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
     // Nuestro modelo de datos
     // Usaremos LiveData para los patrones de observación de datos
-    // The current word
-    val word = MutableLiveData<String>()
+    // The current word, encapsulación solo lectura desde fuera
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
 
-    // The current score
-    val score = MutableLiveData<Int>()
+    // The current score ecpasulación solo lectura
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     lateinit var wordList: MutableList<String>
 
     init {
         // Iniciamos los datos del modelo
-        word.value = ""
-        score.value = 0
+        _word.value = ""
+        _score.value = 0
         // Limpiamos los datos
         resetList()
         nextWord()
@@ -66,12 +71,12 @@ class GameViewModel : ViewModel() {
 
     /** Methods for buttons presses **/
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
@@ -81,7 +86,7 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         if (!wordList.isEmpty()) {
             //Select and remove a word from the list
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
