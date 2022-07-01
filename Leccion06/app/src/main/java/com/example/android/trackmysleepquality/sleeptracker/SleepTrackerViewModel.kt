844f1e3d7f9570dcb83f0ab23749d68ca4f429e8
José Luis González <joseluisgonsan@gmail.com>
2022-07-01 18:28:55 +0200

    Leccion 6.2. Coroutines Collect and display the data

--- Leccion06/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ Leccion06/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -18,7 +18,13 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.viewModelScope
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.formatNights
+import kotlinx.coroutines.launch
 
 /**
  * ViewModel for SleepTrackerFragment.
@@ -26,4 +32,108 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 class SleepTrackerViewModel(
     val database: SleepDatabaseDao,
     application: Application
-) : AndroidViewModel(application)
+) : AndroidViewModel(application) {
+
+    private var tonight = MutableLiveData<SleepNight?>()
+    private val nights = database.getAllNights()
+    val nightsString = Transformations.map(nights) { nights ->
+        formatNights(nights, application.resources)
+    }
+
+    init {
+        initializeTonight()
+    }
+
+    // Lanzamos una tarea en una corrutina
+    // I. You can access the CoroutineScope of a ViewModel through the viewModelScope
+    // El patrón usado
+    private fun initializeTonight() {
+        viewModelScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+        var night = database.getTonight()
+        if (night?.endTimeMilli != night?.startTimeMilli) {
+            night = null
+        }
+        return night
+    }
+
+    fun onStartTracking() {
+        viewModelScope.launch {
+            val newNight = SleepNight()
+            insert(newNight)
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun insert(night: SleepNight) {
+        //  // No need to specify the Dispatcher, Room uses Dispatchers.IO.
+        database.insert(night)
+    }
+
+    fun onStopTracking() {
+        viewModelScope.launch {
+            val oldNight = tonight.value ?: return@launch
+            oldNight.endTimeMilli = System.currentTimeMillis()
+            update(oldNight)
+        }
+    }
+
+    private suspend fun update(night: SleepNight) {
+        database.update(night)
+    }
+
+    fun onClear() {
+        viewModelScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+
+    suspend fun clear() {
+        database.clear()
+    }
+}
+
+/**
+ * Important: Now you should be noticing a pattern:
+
+Launch a coroutine that runs on the main UI thread, because the result from that coroutine affects what is displayed in the UI. You can access the CoroutineScope of a ViewModel through the viewModelScope property of the ViewModel, as shown in the following example:
+Call a suspend function to do the long-running work, so that you don't block the UI thread while waiting for the result.
+The result of the long-running work may affect the UI, but its operation is independent from the UI. For efficiency, switch to the I/O dispatcher, (Room generates his code for you). The I/O dispatcher uses a thread pool that's optimized and set aside for these kinds of operations.
+Then call the long running function to do the work.
+The pattern is shown below.
+
+Without Room
+
+
+fun someWorkNeedsToBeDone {
+viewModelScope.launch {
+suspendFunction()
+}
+}
+
+suspend fun suspendFunction() {
+withContext(Dispatchers.IO) {
+longrunningWork()
+}
+}
+Using Room
+
+
+// Using Room
+fun someWorkNeedsToBeDone {
+viewModelScope.launch {
+suspendDAOFunction()
+}
+}
+
+suspend fun suspendDAOFunction() {
+// No need to specify the Dispatcher, Room uses Dispatchers.IO.
+longrunningDatabaseWork()
+}
+
+ */
