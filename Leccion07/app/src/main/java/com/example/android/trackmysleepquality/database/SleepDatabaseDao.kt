b04f2e77108d27e32d2d427b951eb0920ec12e52
José Luis González <joseluisgonsan@gmail.com>
2022-07-04 13:19:18 +0200

    Leccion 7.3 Implement GridLayout

--- Leccion07/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ Leccion07/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -19,7 +19,6 @@ package com.example.android.trackmysleepquality.database
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 
@@ -71,4 +70,10 @@ interface SleepDatabaseDao {
      */
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
     suspend fun getTonight(): SleepNight?
+
+    /**
+     * Selects and returns the night with given nightId.
+     */
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    fun getNightWithId(key: Long): LiveData<SleepNight>
 }
