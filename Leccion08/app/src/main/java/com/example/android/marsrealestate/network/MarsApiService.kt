075dc8f7a3a97d3ccd13c4c9220d2ced4cbc248c
José Luis González <joseluisgonsan@gmail.com>
2022-07-05 09:23:20 +0200

    Leccion 8.1 Use coroutines with Retrofit

--- Leccion08/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ Leccion08/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -19,7 +19,6 @@ package com.example.android.marsrealestate.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -41,8 +40,9 @@ private val retrofit = Retrofit.Builder()
 
 // Llamadas a las API para obtener los datos
 interface MarsApiService {
+    // Usamos función de suspensón con corrutinas
     @GET("realestate")
-    fun getProperties(): Call<List<MarsProperty>>
+    suspend fun getProperties(): List<MarsProperty>
 }
 
 // Instancia de la APIService Singleton
--- Leccion08/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Leccion08/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,11 +20,9 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.marsrealestate.network.MarsApi
-import com.example.android.marsrealestate.network.MarsProperty
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.launch
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -49,16 +47,14 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        MarsApi.retrofitService.getProperties().enqueue(object : Callback<List<MarsProperty>> {
-
-            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
-                _response.value = "Success: ${response.body()?.size} Mars properties retrieved"
+        // Mucho más limpio con Corrutinas
+        viewModelScope.launch {
+            try {
+                val listResult = MarsApi.retrofitService.getProperties()
+                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+            } catch (e: Exception) {
+                _response.value = "Failure: ${e.message}"
             }
-
-            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
-                _response.value = "Failure: " + t.message
-            }
-
-        })
+        }
     }
 }
