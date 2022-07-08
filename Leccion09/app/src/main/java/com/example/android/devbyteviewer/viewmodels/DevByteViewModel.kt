3cec6851d9f5ac17c1ccaefe6b7215deb4eebd08
José Luis González <joseluisgonsan@gmail.com>
2022-07-08 12:18:35 +0200

    Leccion 9.1: Integrate the repository using a refresh strategy

--- Leccion09/app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
+++ Leccion09/app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
@@ -17,16 +17,10 @@
 package com.example.android.devbyteviewer.viewmodels
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
-import com.example.android.devbyteviewer.domain.DevByteVideo
-import com.example.android.devbyteviewer.network.DevByteNetwork
-import com.example.android.devbyteviewer.network.asDomainModel
-import kotlinx.coroutines.*
+import androidx.lifecycle.*
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import kotlinx.coroutines.launch
 import java.io.IOException
 
 /**
@@ -45,15 +39,14 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
      * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
      * way to set this value to observers.
      */
-    private val _playlist = MutableLiveData<List<DevByteVideo>>()
+    // private val _playlist = MutableLiveData<List<DevByteVideo>>()
 
     /**
      * A playlist of videos that can be shown on the screen. Views should use this to get access
      * to the data.
      */
-    val playlist: LiveData<List<DevByteVideo>>
-        get() = _playlist
-
+//    val playlist: LiveData<List<DevByteVideo>>
+//        get() = _playlist
 
 
     /**
@@ -83,29 +76,58 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
         get() = _isNetworkErrorShown
 
     /**
+     * The data source this ViewModel will fetch results from.
+     */
+    private val videosRepository = VideosRepository(getDatabase(application))
+
+    /**
+     * A playlist of videos displayed on the screen.
+     */
+    val playlist = videosRepository.videos
+
+    /**
      * init{} is called immediately when this ViewModel is created.
      */
     init {
-        refreshDataFromNetwork()
+        refreshDataFromRepository()
     }
 
     /**
      * Refresh data from network and pass it via LiveData. Use a coroutine launch to get to
      * background thread.
      */
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
+//    private fun refreshDataFromNetwork() = viewModelScope.launch {
+//
+//        try {
+//            val playlist = DevByteNetwork.devbytes.getPlaylist()
+//            _playlist.postValue(playlist.asDomainModel())
+//
+//            _eventNetworkError.value = false
+//            _isNetworkErrorShown.value = false
+//
+//        } catch (networkError: IOException) {
+//            // Show a Toast error message and hide the progress bar.
+//            delay(2000) // delay for 2 seconds para ver que se muestra el psin de carga
+//            _eventNetworkError.value = true
+//        }
+//    }
 
-        try {
-             val playlist = DevByteNetwork.devbytes.getPlaylist()
-            _playlist.postValue(playlist.asDomainModel())
+    /**
+     * Refresh data from the repository. Use a coroutine launch to run in a
+     * background thread.
+     */
+    private fun refreshDataFromRepository() {
+        viewModelScope.launch {
+            try {
+                videosRepository.refreshVideos()
+                _eventNetworkError.value = false
+                _isNetworkErrorShown.value = false
 
-            _eventNetworkError.value = false
-            _isNetworkErrorShown.value = false
-
-        } catch (networkError: IOException) {
-            // Show a Toast error message and hide the progress bar.
-            delay(2000) // delay for 2 seconds para ver que se muestra el psin de carga
-            _eventNetworkError.value = true
+            } catch (networkError: IOException) {
+                // Show a Toast error message and hide the progress bar.
+                if (playlist.value.isNullOrEmpty())
+                    _eventNetworkError.value = true
+            }
         }
     }
 
