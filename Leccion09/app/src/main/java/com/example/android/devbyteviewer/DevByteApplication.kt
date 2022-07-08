d68efdfd162282d5fa95248b85cbfad2f3b7cb83
José Luis González <joseluisgonsan@gmail.com>
2022-07-08 12:54:37 +0200

    Leccion 9.2: Define a periodic WorkRequest

--- Leccion09/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ Leccion09/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -17,7 +17,15 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.android.devbyteviewer.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Override application to setup background work via WorkManager
@@ -30,8 +38,35 @@ class DevByteApplication : Application() {
      * Use it to setup any background tasks, running expensive setup operations in a background
      * thread to avoid delaying app start.
      */
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        delayedInit()
+    }
+
+    /**
+     * Setup WorkManager background job to 'fetch' new network data daily.
+     */
+    private fun setupRecurringWork() {
+        //val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS).build()
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(15, TimeUnit.MINUTES).build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            Timber.plant(Timber.DebugTree())
+            setupRecurringWork()
+        }
     }
 }
--- Leccion09/app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWorker.kt
+++ Leccion09/app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWorker.kt
@@ -11,6 +11,10 @@ import timber.log.Timber
 class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
     CoroutineWorker(appContext, params) {
 
+    companion object {
+        const val WORK_NAME = "com.example.android.devbyteviewer.work.RefreshDataWorker"
+    }
+
     override suspend fun doWork(): Result {
         val database = getDatabase(applicationContext)
         val repository = VideosRepository(database)
